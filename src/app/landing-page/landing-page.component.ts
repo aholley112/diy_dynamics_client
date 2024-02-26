5feaac3dda450b8e19f5febcc23a9b5f6cda0b63
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-02-26 15:02:59 -0600

    added landing page
    added landing page

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+
+  constructor(private router: Router) {}
+
+
+}
+
