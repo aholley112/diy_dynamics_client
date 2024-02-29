8a2804133d988bf61c93fc138f6da3f6f4f0b539
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-02-28 19:38:19 -0600

    adding login and sign up

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -3,14 +3,13 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from '../../environments/environment';
 
-
-
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
   constructor(private http: HttpClient, private router: Router) { }
 
+  // Method to send a POST request to the server to log in the user
   login(username: string, password: string) {
     return this.http.post<{token: string}>(`${environment.apiUrl}/login`, {
       username,
@@ -18,28 +17,32 @@ export class AuthenticationService {
     });
   }
 
-  signup(firstName: string, lastName: string, email: string, password: string) {
-    // Adjust the URL and the body as necessary for your backend implementation
+  // Method to send a POST request to the server to sign up the user
+  signup(firstName: string, lastName: string, email: string, username: string, password: string) {
     return this.http.post<{message: string, user: any}>(`${environment.apiUrl}/signup`, {
-      firstName,
-      lastName,
-      email,
-      password
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      username: username,
+      password: password
     });
   }
-
+  // Method to save the JWT token in local storage
   setToken(token: string) {
     localStorage.setItem('token', token);
   }
 
+  // Method to retrieve the JWT token from local storage
   getToken() {
     return localStorage.getItem('token');
   }
 
+  // Method to check if the user is logged in
   isLoggedIn() {
     return !!this.getToken();
   }
 
+  // Method to log out the user. Need to implement in HTML
   logout() {
     localStorage.removeItem('token');
     this.router.navigate(['/login']);
