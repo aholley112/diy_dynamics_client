e4e9388a0a251410f7aa670635802dd17f00356c
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-07 12:20:03 -0600

    removing landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+// import { Component } from '@angular/core';
+// import { Router, RouterModule } from '@angular/router';
 
-@Component({
-  selector: 'app-landing-page',
-  standalone: true,
-  imports: [RouterModule],
-  templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
-})
-export class LandingPageComponent {
+// @Component({
+//   selector: 'app-landing-page',
+//   standalone: true,
+//   imports: [RouterModule],
+//   templateUrl: './landing-page.component.html',
+//   styleUrl: './landing-page.component.scss'
+// })
+// export class LandingPageComponent {
 
-  constructor(private router: Router) {}
+//   constructor(private router: Router) {}
 
 
-}
+// }
 
