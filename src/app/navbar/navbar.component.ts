17dc809c8049d1e09d0f3ebe795e78800855f4ad
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-07 19:41:15 -0600

    added dropdown to navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,12 +2,15 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { Category } from '../shared/models/category.model';
+import { AuthenticationService } from '../core/services/authentication.service';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
@@ -17,15 +20,25 @@ export class NavigationBarComponent {
 
   @Output() search = new EventEmitter<string>();
 
-  constructor(private router: Router){}
+  constructor(public authService: AuthenticationService, private router: Router) {}
 
   searchCategories(): void {
     console.log('Emitting search text:', this.searchText);
     this.search.emit(this.searchText);
   }
 
-  goToLogin(): void {
-    this.router.navigate(['/login']);
+  goToAuth(mode: 'log-in' | 'sign-up'): void {
+    console.log('Attempting to navigate to auth with mode:', mode);
+    this.router.navigate(['/auth'], { queryParams: { action: mode } });
+  }
+
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/']); 
   }
 
   goToProfile(): void {
