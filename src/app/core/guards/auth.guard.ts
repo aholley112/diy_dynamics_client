e6c96969cc8896e2fb75dabb054e2bce3dd476f4
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-07 18:52:34 -0600

    temporarily removed guards for testing

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,21 +1,17 @@
-/*import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import { AuthenticationService } from './services/authentication.service';
-import { ActivatedRoute } from '@angular/router';
-
+import { AuthenticationService } from '../services/authentication.service';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-
   constructor(private authService: AuthenticationService, private router: Router) {}
 
   canActivate(): boolean {
-    if (this.authService.isLoggedIn()) {
-      return true;
-    } else {
-      this.router.navigate(['/auth'], { queryParams: { action: 'log-in' } }); // Adjust as needed
+    if (!this.authService.getToken()) {
+      this.router.navigate(['/auth']);
       return false;
     }
+    return true;
   }
-}*/
+}
--- src/app/core/guards/no-auth.guard.ts
+++ src/app/core/guards/no-auth.guard.ts
@@ -1,32 +1,18 @@
-/*import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
-import { AuthenticationService } from './services/authentication.service';
-
-
-export const noAuthGuard: CanActivateFn = () => {
-  const authService = inject(AuthenticationService);
-
-  return !authService.isLoggedIn();
-
-};*/
-
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NoAuthGuard implements CanActivate {
-
   constructor(private authService: AuthenticationService, private router: Router) {}
 
   canActivate(): boolean {
-    if (!this.authService.isLoggedIn()) {
-      return true;
-    } else {
-      this.router.navigate(['/landing-page']);
+    if (this.authService.getToken()) {
+      this.router.navigate(['/home']);
       return false;
     }
+    return true;
   }
 }
