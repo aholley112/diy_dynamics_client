926197a9c50ac750e78205abd3c9c7e91796e3d2
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-07 18:52:49 -0600

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,37 +1,34 @@
 import { Routes } from '@angular/router';
-/*import { NoAuthGuard } from './no-auth.guard';
-import { AuthGuard } from './auth.guard';*/
+// import { NoAuthGuard } from './core/guards/no-auth.guard';
+// import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
     loadComponent: () => import('./auth/auth.component').then(m => m.AuthComponent),
-    /*canActivate: [NoAuthGuard]*/
-  },
-  {
-    path: 'landing-page',
-    loadComponent: () => import('./landing-page/landing-page.component').then(m => m.LandingPageComponent),
-    /*canActivate: [AuthGuard] // Apply AuthGuard to protect the landing page route and ensure only authenticated users can access it*/
+    // canActivate: [NoAuthGuard]
   },
 
   {
     path: 'home',
     loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
-   // canActivate: [AuthGuard]
+    // canActivate: [AuthGuard]
   },
 
-  {
-    path: 'projects',
-    loadComponent: () => import('./projects/projects.component').then(m => m.ProjectsComponent),
-  },
 
   {
     path: 'categories',
     loadComponent: () => import('./categories/categories.component').then(m => m.CategoriesComponent),
   },
 
-  { path: '', redirectTo: 'landing-page', pathMatch: 'full' },
+    {
+      path: 'profile',
+      loadComponent: () => import('./profile/profile.component').then(m => m.ProfileComponent),
+      // canActivate: [AuthGuard]
+    },
 
+ { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: '**', redirectTo: 'home' }
+
 
 ];
