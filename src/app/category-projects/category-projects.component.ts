b8e360e33ecde331d7e7cb58e0418c39fb866cab
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-08 21:08:10 -0600

    created category-projects component

---
+++ src/app/category-projects/category-projects.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../shared/models/project.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProjectService } from '../core/services/project.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-category-projects',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './category-projects.component.html',
+  styleUrl: './category-projects.component.scss'
+})
+export class CategoryProjectsComponent implements OnInit {
+  projects: Project[] = [];
+
+  constructor(private projectService: ProjectService, private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const categoryId = +params['id'];
+      if (!isNaN(categoryId)) {
+        this.loadProjectsForCategory(categoryId);
+      } else {
+        console.error('Invalid category ID');
+      }
+    });
+  }
+  viewProject(projectId: number): void {
+    this.router.navigate(['/project-detail', projectId]);
+  }
+  
+  loadProjectsForCategory(categoryId: number): void {
+    this.projectService.getProjectsByCategory(categoryId).subscribe({
+      next: (projects) => this.projects = projects,
+      error: (error) => console.error('Error fetching projects for category', error)
+    });
+  }
+}
