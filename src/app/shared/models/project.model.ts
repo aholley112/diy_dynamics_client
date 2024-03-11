2aac83169a2dca6bd40a3708d167b290b9efeebf
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-11 18:18:05 -0500

    resolved error with completion time showing

--- src/app/shared/models/project.model.ts
+++ src/app/shared/models/project.model.ts
@@ -4,7 +4,7 @@ export interface Project {
   description: string;
   isFavoriteProject: boolean;
   instructions: string;
-  estimatedTimeToCompletion: string;
+  est_time_to_completion: string;
   userId: number;
   image_url: string;
 }
