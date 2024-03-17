5fc7e5ad85b38bdb457fd0ecba429e78ad614f14
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-16 21:06:42 -0500

    added getUserProfile

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -2,16 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../../shared/models/user.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl =  'http://localhost:3000';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   getCurrentUser(): Observable<User> {
     return this.http.get<User>(`${this.apiUrl}/current`);
+  }
+
+  getUserProfile(userId: number): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}/users/${userId}/profile`);
   }
 }
