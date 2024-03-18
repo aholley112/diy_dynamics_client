084adc652734f5348839bde45221e0a937bda572
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-17 19:51:43 -0500

    updated est_time_to_completion

--- src/app/shared/models/project.model.ts
+++ src/app/shared/models/project.model.ts
@@ -4,7 +4,9 @@ export interface Project {
   description: string;
   isFavoriteProject: boolean;
   instructions: string;
-  estimatedTimeToCompletion: string;
+  est_time_to_completion: string;
   userId: number;
   image_url: string;
+  material_names: string[];
+  tool_names: string[];
 }
