5fd71ab395fb34a29d89b6ec8a1989891b525cf0
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-20 18:20:25 -0500

    created user component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../core/services/admin.service';
+import { Category } from '../shared/models/category.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss'
+})
+export class UserComponent implements OnInit {
+  categories: Category[] = []; // Array to store categories
+
+  constructor(private adminService: AdminService) { }
+
+  ngOnInit(): void {
+    this.loadCategories();
+  }
+
+  loadCategories(): void {
+    this.adminService.getCategories().subscribe(
+      (data) => {
+        this.categories = data;
+      },
+      (error) => {
+        console.error('Error loading categories:', error);
+      }
+    );
+  }
+}
