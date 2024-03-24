d39e5fe59ad337e22194c3a5bfaac752079e0173
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-23 21:38:27 -0500

    testing image

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,36 +1,36 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Project } from '../shared/models/project.model';
-// import { FormsModule } from '@angular/forms';
-// import { CommonModule } from '@angular/common';
-// import { ProjectService } from '../core/services/project.service';
-// import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../shared/models/project.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ProjectService } from '../core/services/project.service';
+import { Router } from '@angular/router';
 
 
-// @Component({
-//   selector: 'app-projects',
-//   standalone: true,
-//   imports: [FormsModule, CommonModule],
-//   templateUrl: './projects.component.html',
-//   styleUrl: './projects.component.scss'
-// })
-// export class ProjectsComponent implements OnInit {
-//   projects: Project[] = [];
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent implements OnInit {
+  projects: Project[] = [];
 
-//   constructor(private projectService: ProjectService) {}
+  constructor(private projectService: ProjectService) {}
 
-//   ngOnInit(): void {
-//     this.loadProjects();
-//   }
+  ngOnInit(): void {
+    this.loadProjects();
+  }
 
-//   loadProjects(): void {
-//     this.projectService.getProjects().subscribe(
-//       (projects) => {
-//         this.projects = projects;
-//       },
-//       (error) => {
-//         console.error('Error fetching projects', error);
-//       }
-//     );
-//   }
-// }
+  loadProjects(): void {
+    this.projectService.getProjects().subscribe(
+      (projects) => {
+        this.projects = projects;
+      },
+      (error) => {
+        console.error('Error fetching projects', error);
+      }
+    );
+  }
+}
 
