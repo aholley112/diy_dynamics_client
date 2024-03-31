40cb94119442329cbd7b700d76de2aba62227db5
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-03-30 20:16:15 -0500

    added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
+import { NavigationBarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, LazyLoadImageModule, NavigationBarComponent],
 })
 export class AppComponent {
   title = 'diy_dynamics_client';
