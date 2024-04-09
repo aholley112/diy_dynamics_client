2c04736c978d49811ea4f4cfd549ca5f7e29b0b5
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-04-09 10:39:26 -0500

    resolving auth form issue

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -1,7 +1,7 @@
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthenticationService } from '../../core/services/authentication.service';
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
@@ -24,6 +24,14 @@ export class AuthComponent implements OnInit {
 
   @Output() onFormClose = new EventEmitter<void>();
 
+  @Input() set authAction(action: 'sign-up' | 'log-in') {
+    this.isSignInMode = action === 'log-in';
+    if (this.authForm) {
+      this.authForm.reset();
+      this.adjustFormValidators();
+    }
+  }
+
   constructor(
     private fb: FormBuilder,
     private authenticationService: AuthenticationService,
@@ -32,17 +40,10 @@ export class AuthComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    // Initialize the form
     this.initializeForm();
-    this.route.queryParams.subscribe(params => {
-      const mode = params['action'];
-      // Setting the authentication mode based on the query parameter
-      this.isSignInMode = mode !== 'sign-up';
-      this.adjustFormValidators();
-    });
   }
 
-  // Initializing the fom with default values and validators
+  // Method to initialize the form
   initializeForm() {
     this.authForm = this.fb.group({
       firstName: [''],
@@ -51,89 +52,86 @@ export class AuthComponent implements OnInit {
       username: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
-  }
-
-// Adjusting the form validators based on the authentication mode
-  adjustFormValidators() {
-    const controls = this.authForm.controls;
-    if (!this.isSignInMode) {
-      controls['firstName'].setValidators([Validators.required]);
-      controls['lastName'].setValidators([Validators.required]);
-      controls['email'].setValidators([Validators.required, Validators.email]);
-    } else {
-      controls['firstName'].clearValidators();
-      controls['lastName'].clearValidators();
-      controls['email'].clearValidators();
-    }
-    controls['firstName'].updateValueAndValidity();
-    controls['lastName'].updateValueAndValidity();
-    controls['email'].updateValueAndValidity();
-  }
-
-  // Toggling the authentication mode
-  toggleAuthMode() {
-    this.isSignInMode = !this.isSignInMode;
-    this.errorMsg = '';
-    this.successMessage = '';
-    this.formSubmitted = false;
-    this.authForm.reset();
     this.adjustFormValidators();
   }
 
-  // Handling the form submission
-  onAuthSubmit(): void {
-    this.formSubmitted = true;
-   if (!this.authForm.valid) {
-      return;
-    }
-
-    const username = this.authForm.get('username')?.value;
-    const password = this.authForm.get('password')?.value;
-
-    if (this.isSignInMode) {
-      this.login(username, password);
-    } else {
-      const firstName = this.authForm.get('firstName')?.value;
-      const lastName = this.authForm.get('lastName')?.value;
-      const email = this.authForm.get('email')?.value;
-      this.signup(firstName, lastName, email, username, password);
+  // Method to adjust form validators based on the current mode
+  adjustFormValidators() {
+    if (this.authForm) {
+      const controls = this.authForm.controls;
+      if (!this.isSignInMode) {
+        controls['firstName'].setValidators([Validators.required]);
+        controls['lastName'].setValidators([Validators.required]);
+        controls['email'].setValidators([Validators.required, Validators.email]);
+      } else {
+        controls['firstName'].clearValidators();
+        controls['lastName'].clearValidators();
+        controls['email'].clearValidators();
+      }
+      controls['firstName'].updateValueAndValidity();
+      controls['lastName'].updateValueAndValidity();
+      controls['email'].updateValueAndValidity();
     }
   }
-  // Logging in
-// Logging in
-login(username: string, password: string): void {
-  this.isLoading = true;
 
+  // Method to toggle between sign-in and sign-up modes
+  toggleAuthMode() {
+    this.isSignInMode = !this.isSignInMode;
+    this.adjustFormValidators();
+  }
+
+  // Method to handle form submission
+  onAuthSubmit() {
+    this.formSubmitted = true;
+    if (this.authForm.valid) {
+      const username = this.authForm.get('username')?.value;
+      const password = this.authForm.get('password')?.value;
+      if (this.isSignInMode) {
+        this.login(username, password);
+      } else {
+        const firstName = this.authForm.get('firstName')?.value;
+        const lastName = this.authForm.get('lastName')?.value;
+        const email = this.authForm.get('email')?.value;
+        this.signup(firstName, lastName, email, username, password);
+      }
+    }
+  }
+
+// Method to handle login
+ login(username: string, password: string): void {
+  this.isLoading = true;
   this.authenticationService.login(username, password).subscribe({
     next: (res) => {
-        this.isLoading = false;
-        this.authenticationService.setToken(res.token);
-        localStorage.setItem('currentUser', JSON.stringify(res.user));
-        this.onFormClose.emit();
-        this.router.navigate(['/home']);
-      },
+      this.isLoading = false;
+      this.authenticationService.setToken(res.token);
+      localStorage.setItem('currentUser', JSON.stringify(res.user));
+      this.onFormClose.emit();
+      this.router.navigate(['/home']);
+    },
     error: (err) => {
       this.isLoading = false;
-      this.errorMsg = 'Failed to login: ' + err.message;
+      this.errorMsg = 'Failed to login: ' + err.error.message;
     }
   });
 }
 
-
-  // Signing up
-  signup(firstName: string, lastName: string, email: string, username: string, password: string) {
-    this.authenticationService.signup(firstName, lastName, email, username, password).subscribe({
-      next: (res) => {
-        this.successMessage = 'Signup successful! Please log in with your new credentials.';
-        this.errorMsg = '';
-        this.formSubmitted = false;
-        this.isSignInMode = true;
-        this.authForm.patchValue({ username, password: '' });
-      },
-      error: (err) => {
-        this.errorMsg = 'Failed to sign up: ' + err.message;
-        this.formSubmitted = false;
-      }
-    });
-  }
+// Private method to handle signup
+signup(firstName: string, lastName: string, email: string, username: string, password: string): void {
+  this.isLoading = true;
+  this.authenticationService.signup(firstName, lastName, email, username, password).subscribe({
+    next: (res) => {
+      this.isLoading = false;
+      this.successMessage = 'Signup successful! Please log in with your new credentials.';
+      this.errorMsg = '';
+      this.formSubmitted = false;
+      this.isSignInMode = true;
+      this.authForm.patchValue({ username, password: '' });
+    },
+    error: (err) => {
+      this.isLoading = false;
+      this.errorMsg = 'Failed to sign up: ' + err.error.message;
+      this.formSubmitted = false;
+    }
+  });
+}
 }
