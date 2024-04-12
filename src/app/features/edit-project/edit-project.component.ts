58e87fa8a0a0606f3a251d15095f7a0f508b95ef
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-04-12 18:08:43 -0500

    adjusting layout and SCSS

--- src/app/features/edit-project/edit-project.component.ts
+++ src/app/features/edit-project/edit-project.component.ts
@@ -28,17 +28,20 @@ export class EditProjectComponent implements OnInit {
     private fb: FormBuilder,
     private router: Router,
     private categoryService: CategoryService,
-
   ) {
+
+    // Initialize the form
     this.editForm = this.fb.group({
       title: ['', Validators.required],
-      category: ['', Validators.required],
       description: [''],
       instructions: [''],
-      estTime: [''],
-      materials: [''],
-      tools: [''],
+      est_time_to_completion: [''],
+      material_names: [''],
+      tool_names: [''],
+      category: [''],
+      image: ['']
     });
+
   }
 
   ngOnInit(): void {
@@ -49,6 +52,7 @@ export class EditProjectComponent implements OnInit {
     });
   }
 
+  // Method to load categories for the dropdown list in the edit form.
   loadCategories(): void {
     this.categoryService.getCategories().subscribe(
       categories => {
@@ -60,27 +64,33 @@ export class EditProjectComponent implements OnInit {
     );
   }
 
+  // Method to load project data into the form.
   loadProjectData(): void {
     this.projectService.getProjectById(this.projectId).subscribe({
       next: project => {
+        console.log("Received project data:", project);
         this.editForm.patchValue({
           title: project.title,
           description: project.description,
           instructions: project.instructions,
-          estTime: project.est_time_to_completion,
-          materials: project.material_names,
-          tools: project.tool_names
+          est_time_to_completion: project.est_time_to_completion,
+          material_names: project.material_names || '',
+          tool_names: project.tool_names || '',
+          image: project.image_url,
+          category: project.categories && project.categories.length > 0 ? project.categories[0].id : null
         });
 
         if (project.categories && project.categories.length > 0) {
-          this.categoryId = project.categories[0].id;
+          this.editForm.patchValue({
+            category: project.categories[0].id
+          });
         }
       },
       error: error => console.error('Error fetching project', error)
     });
   }
 
-
+  // Method to handle file selection
   onFileSelected(event: Event): void {
     const target = event.target as HTMLInputElement;
     if (target.files && target.files.length) {
@@ -88,31 +98,40 @@ export class EditProjectComponent implements OnInit {
     }
   }
 
+  // Method to handle form submission
   saveChanges(): void {
-    if (!this.editForm.valid) {
-      console.error('Form is not valid');
-      return;
+    console.log('Form validity:', this.editForm.valid);
+
+    if (this.editForm.valid) {
+      const formData = new FormData();
+      formData.append('title', this.editForm.value.title);
+      formData.append('description', this.editForm.value.description);
+      formData.append('instructions', this.editForm.value.instructions);
+      formData.append('est_time_to_completion', this.editForm.value.est_time_to_completion);
+      formData.append('material_names', this.editForm.value.material_names);
+      formData.append('tool_names', this.editForm.value.tool_names);
+
+      if (this.selectedFile) {
+          formData.append('image', this.selectedFile);
+      }
+
+
+      if (this.editForm.value.category) {
+          formData.append('category_id', this.editForm.value.category);
+      }
+
+      // Call the updateProject method from the project service
+      this.projectService.updateProject(this.projectId, formData).subscribe({
+        next: (response) => {
+          console.log('Project updated successfully:', response);
+          this.router.navigate(['/project-detail', this.projectId]);
+        },
+        error: (error) => {
+          console.error('There was an error updating the project:', error);
+        }
+      });
+    } else {
+      console.error('Form is not valid:', this.editForm.errors);
     }
-
-    const formData = new FormData();
-    formData.append('title', this.editForm.value.title);
-    formData.append('description', this.editForm.value.description);
-    formData.append('instructions', this.editForm.value.instructions);
-    formData.append('est_time_to_completion', this.editForm.value.estTime);
-    formData.append('material_names', this.editForm.value.materials);
-    formData.append('tool_names', this.editForm.value.tools);
-
-    if (this.categoryId) {
-      formData.append('category_id', this.categoryId.toString());
-    }
-
-    if (this.selectedFile) {
-      formData.append('image', this.selectedFile);
-    }
-
-    this.projectService.updateProject(this.projectId, formData).subscribe({
-      next: () => this.router.navigate(['/project-detail', this.projectId]),
-      error: error => console.error('There was an error!', error)
-    });
   }
 }
--- src/app/features/project-detail/project-detail.component.ts
+++ src/app/features/project-detail/project-detail.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { ProjectService } from '../../core/services/project.service';
 import { Project } from '../../shared/models/project.model';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../core/services/user.service';
 import { AuthenticationService } from '../../core/services/authentication.service';
+import { Comment } from '../../shared/models/comment.model';
+import { FormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-project-detail',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule, MatTooltipModule],
   templateUrl: './project-detail.component.html',
   styleUrl: './project-detail.component.scss'
 })
@@ -19,12 +22,22 @@ export class ProjectDetailComponent implements OnInit {
   currentUserId: number | null = null;
   favoriteId: number | null = null;
   favoriteProjectIds: number[] = [];
+  comments: Comment[] = [];
+  newCommentText: string = '';
+  editCommentId: number | null = null;
+  currentlyEditingCommentText: string = '';
+  likeIds: { [projectId: number]: number } = {};
+  showCommentBox: boolean = false;
+
+  @ViewChild('commentInput') commentInputRef!: ElementRef;
+
 
   constructor(
     private route: ActivatedRoute,
     private projectService: ProjectService,
     private userService: UserService,
     private authenticationService: AuthenticationService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -46,6 +59,7 @@ export class ProjectDetailComponent implements OnInit {
         this.project = project;
         this.isFavorite = project.is_favorite_project;
         this.favoriteId = project.favorite_id;
+        this.fetchComments(project.id);
 
         project.material_names = Array.isArray(project.material_names)
         ? project.material_names.join(', ')
@@ -59,6 +73,15 @@ export class ProjectDetailComponent implements OnInit {
     });
   }
 
+  // Method to toggle the visibility of the comment box
+  toggleCommentBox(): void {
+    this.showCommentBox = !this.showCommentBox;
+    if (this.showCommentBox) {
+      setTimeout(() => this.focusCommentInput(), 0);
+    }
+  }
+
+  // Method to check if the project is a favorite
   isProjectFavorite(projectId: number): boolean {
     return this.favoriteProjectIds.includes(projectId);
   }
@@ -80,6 +103,7 @@ export class ProjectDetailComponent implements OnInit {
     }
   }
 
+
   // Method to remove the project from the user's favorites
   removeFromFavorites(): void {
     if (this.project && this.favoriteId != null) {
@@ -96,4 +120,117 @@ export class ProjectDetailComponent implements OnInit {
     }
   }
 
+  // Method to toggle the favorite status of the project
+
+  toggleFavorite(): void {
+    if (this.isFavorite) {
+      this.removeFromFavorites();
+    } else {
+      this.addToFavorites();
+    }
   }
+
+  // Method to fetch the comments for the project
+  fetchComments(projectId: number): void {
+    this.projectService.getCommentsByProjectId(projectId).subscribe({
+      next: (comments) => {
+        this.comments = comments;
+      },
+      error: (error) => console.error('Error fetching comments', error)
+    });
+  }
+
+  // Method to add a comment to the project
+  addComment(): void {
+    if (this.project && this.newCommentText.trim()) {
+      this.projectService.addComment(this.project.id, { text: this.newCommentText.trim() }).subscribe({
+        next: (comment) => {
+          console.log('New comment added:', comment);
+          this.comments.push(comment);
+          this.newCommentText = '';
+        },
+        error: (error) => console.error('Error adding comment', error)
+      });
+    }
+  }
+
+  // Method to start editing a comment
+  startEditComment(comment: Comment): void {
+    this.editCommentId = comment.id;
+    this.currentlyEditingCommentText = comment.text;
+  }
+
+  // Method to save the edited comment
+  saveEditedComment(comment: Comment): void {
+    if (!this.editCommentId || !this.project) return;
+    const updatedCommentData = { text: this.currentlyEditingCommentText };
+    this.projectService.updateComment(this.project.id, this.editCommentId, updatedCommentData)
+      .subscribe({
+        next: (updatedComment) => {
+          const index = this.comments.findIndex(c => c.id === updatedComment.id);
+          if (index !== -1) {
+            this.comments[index] = updatedComment;
+          }
+          this.editCommentId = null;
+          this.currentlyEditingCommentText = '';
+        },
+        error: (error) => console.error('Error updating comment', error)
+      });
+  }
+
+  // Method to delete a comment
+  deleteComment(commentId: number): void {
+    if (this.project) {
+      this.projectService.deleteComment(this.project.id, commentId).subscribe({
+        next: () => {
+          this.comments = this.comments.filter(comment => comment.id !== commentId);
+        },
+        error: (error) => console.error('Error deleting comment', error)
+      });
+    }
+  }
+
+  // Method to like or unlike a project
+  toggleLike(projectId: number): void {
+    if (this.project) {
+      if (this.project.isLiked) {
+        const likeId = this.likeIds[this.project.id];
+        if (likeId !== undefined) {
+          this.projectService.removeLike(likeId).subscribe(() => {
+            if (this.project) {
+              this.project.isLiked = false;
+              this.clearLikeIdForProject(this.project.id);
+            }
+          });
+        }
+      } else {
+        this.projectService.addLike(projectId).subscribe(like => {
+          if (this.project) {
+            this.project.isLiked = true;
+            this.storeLikeIdForProject(this.project.id, like.id);
+          }
+        });
+      }
+    }
+  }
+
+  // Method to focus the comment input.
+  focusCommentInput(): void {
+    this.commentInputRef.nativeElement.focus();
+  }
+
+  // Method to store the like ID for a project
+  storeLikeIdForProject(projectId: number, likeId: number): void {
+    this.likeIds[projectId] = likeId;
+  }
+
+  // Method to get the like ID for a project
+  getLikeIdByProjectId(projectId: number): number | null {
+    return this.likeIds[projectId] || null;
+  }
+
+  // Method to clear the like ID for a project
+  clearLikeIdForProject(projectId: number): void {
+    delete this.likeIds[projectId];
+  }
+}
