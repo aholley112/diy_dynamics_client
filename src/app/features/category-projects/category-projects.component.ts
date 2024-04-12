15980a8c1006a697691995531c6e13a664531617
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-04-12 17:37:46 -0500

    adjusting SCSS

--- src/app/features/category-projects/category-projects.component.ts
+++ src/app/features/category-projects/category-projects.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../../shared/models/project.model';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { ProjectService } from '../../core/services/project.service';
 import { CommonModule } from '@angular/common';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
@@ -8,17 +8,15 @@ import { LazyLoadImageModule } from 'ng-lazyload-image';
 @Component({
   selector: 'app-category-projects',
   standalone: true,
-  imports: [CommonModule, LazyLoadImageModule],
+  imports: [CommonModule, LazyLoadImageModule, RouterModule],
   templateUrl: './category-projects.component.html',
   styleUrl: './category-projects.component.scss'
 })
 export class CategoryProjectsComponent implements OnInit {
-  // The list of projects to be displayed
   projects: Project[] = [];
 
   constructor(private projectService: ProjectService, private route: ActivatedRoute, private router: Router) {}
 
-  // Fetch the projects when the component is initialized
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       const categoryId = +params['category_id'];
@@ -32,6 +30,13 @@ export class CategoryProjectsComponent implements OnInit {
   // Method to navigate to project detail page
   viewProject(projectId: number): void {
     this.router.navigate(['/project-detail', projectId]);
+  }
+
+  // Callback for when project images have loaded.
+  onImageLoad(project: Project): void {
+    setTimeout(() => {
+      project.is_loading = false;
+    }, 5000);
   }
 
   // Method to fetch the projects from the API
